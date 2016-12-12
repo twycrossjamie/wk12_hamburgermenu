400b3a822ab80b0dfaf2230d5f2a43a6b23c5a60
Jamie Twycross <2twycj12@solent.ac.uk>
2016-12-12 12:40:32 +0000

    compiled to sass

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+ $(document).ready(function(){
+    
+        
+		var pull = $('#pull');
+        menu = $('nav ul');
+
+		$(pull).on('click', function(e) {
+			e.preventDefault();
+			menu.slideToggle();
+		});
+		
+		$(window).resize(function(){
+			var w = $(this).width();
+
+			if(w > 800 && menu.is(':hidden')) {
+				menu.removeAttr('style');
+			}
+		});
+
+		$('li').on('click', function(e) {				
+			var w = $(window).width();
+			if(w < 800 ) {
+				menu.slideToggle();
+			}
+		});
+
+		$('.panel').height($(window).height());
+		
+
+    
+    
+    });
